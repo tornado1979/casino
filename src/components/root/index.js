f162497dcd18538724bf5b096191f0a0b80645ca
Theodore Itzaris <theodoreme@gmail.com>
2018-03-22 15:16:52 +0200

    Add Root component that wraps Router

---
+++ src/components/root/index.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { Provider } from 'react-redux'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import PropTypes from 'prop-types'
+import Games from '../../modules/gameLobby'
+import Header from '../../components/header'
+import Footer from '../../components/footer'
+import { routes } from '../../routes'
+
+const Root = ({ store }) => (
+  <Provider store={store}>
+    <Router>
+      <div className="wrapper">
+        <Header/>
+        <div className="main">
+          <Route exact path="/games" component={Games} />
+          <Route path="/games/:gameId" component={Games}/>
+        </div>
+        <Footer/>
+      </div>
+    </Router>
+  </Provider>
+)
+ 
+Root.propTypes = {
+  store: PropTypes.object.isRequired
+}
+ 
+export default Root
