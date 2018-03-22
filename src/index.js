ff2b1d023cad41aea531156eaf120715e9a5a58f
Theodore Itzaris <theodoreme@gmail.com>
2018-03-22 15:22:28 +0200

    update root index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+
+import Root from './components/root'
+import store from './store'
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Root store={store}/>,
+  document.getElementById('root')
+);
 registerServiceWorker();
