2a6e405e2bec6f65680e936d83bd1e53d52b9f5a
Theodore Itzaris <theodoreme@gmail.com>
2018-03-23 13:35:59 +0200

    add login route

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,11 +1,16 @@
 import React from 'react'
+import { connect } from 'react-redux'
 import { Link } from 'react-router-dom';
 
-export default () => (
+import { getUser } from '../../modules/user/selectors'
+
+const Header = () => (
   <nav className="nav justify-content-end flex-column flex-sm-row">
     <Link to="/games" className="flex-sm-fill text-sm-center nav-link active" >Games</Link>
     <Link to="/" className="flex-sm-fill text-sm-center nav-link" >Username</Link>
     <Link to="/settings" className="flex-sm-fill text-sm-center nav-link" >Settings</Link>
-    <Link to="/" className="flex-sm-fill text-sm-center nav-link" >Logout</Link>
+    <Link to="/login" className="flex-sm-fill text-sm-center nav-link" >Login</Link>
   </nav>
 )
+
+export default Header
--- src/components/root/index.js
+++ src/components/root/index.js
@@ -1,19 +1,22 @@
 import React from 'react'
 import { Provider } from 'react-redux'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { Router, Route } from 'react-router-dom'
 import PropTypes from 'prop-types'
 import Games from '../../modules/gameLobby'
+import Login from '../../modules/login'
 import Header from '../../components/header'
 import Footer from '../../components/footer'
+import { history } from '../../helpers/history'
 
 const Root = ({ store }) => (
   <Provider store={store}>
-    <Router>
+    <Router history={history}>
       <div className="wrapper">
         <Header/>
         <div className="main">
           <Route exact path="/games" component={Games} />
           <Route path="/games/:gameId" component={Games}/>
+          <Route path="/login" component={Login}/>
         </div>
         <Footer/>
       </div>
