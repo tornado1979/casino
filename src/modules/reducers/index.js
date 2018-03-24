44836cf41e3252ebd03bf18abf60d20b221d7166
Theodore Itzaris <theodoreme@gmail.com>
2018-03-24 12:50:16 +0200

    add GamPlayer reducer into root redurer

--- src/modules/reducers/index.js
+++ src/modules/reducers/index.js
@@ -3,11 +3,17 @@ import { combineReducers } from 'redux'
 import { 
   asyncGames as games,
  } from '../gameLobby/reducers'
+
  import {
    user,
  } from '../user/reducers'
 
+ import {
+   gamePlayer,
+ } from '../gamePlayer/reducers'
+
 const rootReducers = combineReducers({
+  gamePlayer,
   games,
   user,
 })
