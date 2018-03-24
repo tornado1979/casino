31da714b6e2e98fe39ee146d80c8cd9a8f5e23c3
Theodore Itzaris <theodoreme@gmail.com>
2018-03-24 12:51:59 +0200

    refactor user selector

--- src/modules/user/selectors/user.selectors.js
+++ src/modules/user/selectors/user.selectors.js
@@ -1,8 +1,5 @@
 import _ from 'lodash'
 
-export const getUser = (state) => {
-  console.log('is user',state)
-  return (state && !_.isEmpty(state.user))
-}
+export const getUser = (state) => (state && !_.isEmpty(state.user))
 
 export const getUsername = (state) => state && state.user.username
