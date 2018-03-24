eab830180ba7c465aec2d3016f05e4f7b35c3f3a
Theodore Itzaris <theodoreme@gmail.com>
2018-03-24 13:44:48 +0200

    add buttons on gameBrowser
    - Play for fun, message when logged out
    - Play for real, message when logged in
    - the messages are shown on GameLobby, on each game thumbnail

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,12 @@
+//constants for API
 export const GAMES_URL = `http://www.deutz-hellas.gr/dummy-database/games.php`
 export const GAME_URL = `http://www.deutz-hellas.gr/dummy-database/game.php?gameId=`
 export const USERS_URL = `http://localhost/dummy-database/users.php`
 
+//constants for button on GamePlayer
 export const LOGIN_TO_PLAY = 'You need to login to play for real'
 export const GAME_NOT_EXIST = 'Sorry! that game no longer exists'
+
+//constants for GameLobby
+export const PLAY_FOR_FUN = 'Play for fun.'
+export const PLAY_FOR_REAL = 'Play for real.'
