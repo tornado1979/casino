06af9f57bf8dd78209f0894b3d7e592d488b6890
Theodore Itzaris <theodoreme@gmail.com>
2018-03-26 16:04:22 +0300

    remove old tests

--- src/components/main/index.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Main from './index';
-
-it('renders `Main` component without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Main />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
