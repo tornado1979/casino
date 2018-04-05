4b1f9316ece0e3dc19d868bcc972e2607dc3210f
Theodore Itzaris <theodoreme@gmail.com>
2018-04-05 19:49:06 +0300

    add unit tests on async actionCreators with moxios

--- src/modules/gameLobby/actionCreators/gameLobby.actionCreators.spec.js
+++ src/modules/gameLobby/actionCreators/gameLobby.actionCreators.spec.js
@@ -1,5 +1,8 @@
 import { describe, it } from 'mocha'
 import { expect } from 'chai'
+import configureMockStore from 'redux-mock-store'
+import thunk from 'redux-thunk'
+import moxios from 'moxios'
 
 import * as action from './index'
 
@@ -9,7 +12,75 @@ import {
   RECEIVE_GAMES_FAIL,
 } from '../actions'
 
+const middlewares = [thunk]
+const mockStore = configureMockStore(middlewares)
+
 describe('* gameLobby actionCreators:', () => {
+  beforeEach(() => {
+    moxios.install()
+  })
+  afterEach(() => {
+    moxios.uninstall()
+  })
+
+  it('should dispatch `REQUEST_GAMES` and `RECEIVE_GAMES` on fetchGames', () => {
+    const payload = {
+      casinoGames: [
+        {
+          description: 'Lorem ipsum dolor..',
+          favorite: 5,
+          gameId: 1,
+          gameName: 'Finn and the Swirly Spin',
+          image: 'url',
+        },
+      ],
+    }
+    moxios.wait(() => {
+      const request = moxios.requests.mostRecent()
+      request.respondWith({
+        response: payload,
+        status: 200,
+      })
+    })
+
+    const expectedActions = [REQUEST_GAMES, RECEIVE_GAMES]
+
+    const store = mockStore()
+
+    // call the fetchGames() async action creator
+    return store.dispatch(action.fetchGames()).then(() => {
+      const dispatchedActions = store.getActions()
+
+      const actionTypes = dispatchedActions.map(act => act.type)
+
+      expect(actionTypes).eql(expectedActions)
+      expect(dispatchedActions).eql(
+        [
+          {
+            payload: { isFetching: true }, type: REQUEST_GAMES,
+          },
+          {
+            payload: {
+              casinoGames: [
+                {
+                  description: 'Lorem ipsum dolor..',
+                  favorite: 5,
+                  gameId: 1,
+                  gameName: 'Finn and the Swirly Spin',
+                  image: 'url',
+                },
+              ],
+              isFetching: false,
+            },
+            type: RECEIVE_GAMES,
+          },
+        ],
+      )
+    },
+    )
+  })
+
+
   it('should create an action to request games ', () => {
     const expectedAction = {
       payload: { isFetching: true },
--- src/modules/gamePlayer/actionCreators/gamePlayer.actionCreators.spec.js
+++ src/modules/gamePlayer/actionCreators/gamePlayer.actionCreators.spec.js
@@ -1,5 +1,8 @@
 import { describe, it } from 'mocha'
 import { expect } from 'chai'
+import configureMockStore from 'redux-mock-store'
+import thunk from 'redux-thunk'
+import moxios from 'moxios'
 
 import * as action from './index'
 
@@ -9,8 +12,73 @@ import {
   RECEIVE_GAME_FAIL,
 } from '../actions'
 
+const middlewares = [thunk]
+const mockStore = configureMockStore(middlewares)
 
 describe('* gamePlayer actionCreators:', () => {
+  beforeEach(() => {
+    moxios.install()
+  })
+  afterEach(() => {
+    moxios.uninstall()
+  })
+
+  it('should dispatch `REQUEST_GAME` and `RECEIVE_GAME` on fetchGame', () => {
+    const payload = {
+      game: {
+        description: 'Lorem ipsum dolor..',
+        favorite: 5,
+        gameId: 1,
+        gameName: 'Finn and the Swirly Spin',
+        image: 'url',
+      },
+    }
+
+    moxios.wait(() => {
+      const request = moxios.requests.mostRecent()
+      request.respondWith({
+        response: payload,
+        status: 200,
+      })
+    })
+
+    const expectedActions = [REQUEST_GAME, RECEIVE_GAME]
+
+    const store = mockStore()
+
+    // call the fetchGame() async action creator
+    return store.dispatch(action.fetchGame()).then(() => {
+      const dispatchedActions = store.getActions()
+
+      const actionTypes = dispatchedActions.map(act => act.type)
+
+      expect(actionTypes).eql(expectedActions)
+      expect(dispatchedActions).eql(
+        [
+          {
+            payload: { game: {}, isFetching: true }, type: REQUEST_GAME,
+          },
+          {
+            payload: {
+              game: {
+                game: {
+                  description: 'Lorem ipsum dolor..',
+                  favorite: 5,
+                  gameId: 1,
+                  gameName: 'Finn and the Swirly Spin',
+                  image: 'url',
+                },
+              },
+              isFetching: false,
+            },
+            type: RECEIVE_GAME,
+          },
+        ],
+      )
+    },
+    )
+  })
+
   it('should create an action to request game ', () => {
     const expectedAction = {
       payload: {
